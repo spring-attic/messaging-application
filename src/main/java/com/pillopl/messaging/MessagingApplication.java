251901cbe8fb9de0db9b4fbbdc39f68c1bbf0dc9
Jakub Pilimon <jpilimon@9730.local>
2018-03-30 12:31:27 +0200

    initial sources

---
+++ src/main/java/com/pillopl/messaging/MessagingApplication.java
@@ -0,0 +1,12 @@
+package com.pillopl.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApplication.class, args);
+	}
+}
