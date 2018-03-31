21886f9a09e0c135aa80e10974d7c46ca06f5005
jpilimon <jpilimon@pivotal.io>
2018-03-31 12:11:40 +0200

    spring cloud stream enabled with rabbitmq

--- src/main/java/com/pillopl/messaging/MessagingApplication.java
+++ src/main/java/com/pillopl/messaging/MessagingApplication.java
@@ -2,11 +2,16 @@ package com.pillopl.messaging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
+@EnableBinding(Source.class)
 public class MessagingApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(MessagingApplication.class, args);
 	}
+
 }
+
