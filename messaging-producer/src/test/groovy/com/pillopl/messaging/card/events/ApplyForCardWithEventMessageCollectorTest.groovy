975418dee3b1a44e51a887c12f749dfe1ead4a5c
Jakub Pilimon <jakub.pilimon@gmail.com>
2018-04-07 10:11:56 +0200

    even more initail sources

--- messaging-producer/src/test/groovy/com/pillopl/messaging/card/events/ApplyForCardWithEventMessageCollectorTest.groovy
+++ messaging-producer/src/test/groovy/com/pillopl/messaging/card/events/ApplyForCardWithEventMessageCollectorTest.groovy
@@ -1,15 +1,35 @@
 package com.pillopl.messaging.card.events
 
+import com.pillopl.messaging.card.ui.CreditCardApplicationController
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.stream.messaging.Source
+import org.springframework.cloud.stream.test.binder.MessageCollector
+import org.springframework.messaging.support.GenericMessage
 import spock.lang.Specification
+
+import java.util.concurrent.BlockingQueue
 /**
  * @author Jakub Pilimon
  */
 @SpringBootTest
 class ApplyForCardWithEventMessageCollectorTest extends Specification {
 
+    @Autowired MessageCollector messageCollector
+
+    @Autowired Source source
+
+    BlockingQueue<GenericMessage> events
+
+    @Autowired CreditCardApplicationController creditCardApplicationController
+
+
+    def setup() {
+        events = messageCollector.forChannel(source.output())
+    }
 
     def 'should be able to get card when born in 70s or later'() {
+
 
     }
 
--- messaging-producer/src/test/java/com/pillopl/messaging/card/ui/ApplyForCardWithEventMockBeanTest.java
+++ messaging-producer/src/test/java/com/pillopl/messaging/card/ui/ApplyForCardWithEventMockBeanTest.java
@@ -2,6 +2,7 @@ package com.pillopl.messaging.card.ui;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,6 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplyForCardWithEventMockBeanTest {
 
+    @Autowired
+    CreditCardApplicationController creditCardApplicationController;
 
 
     @Test
